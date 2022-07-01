52c0ec324048fc343a5408f463d17d0621890199
Vivyen Vu <pokeviv@gmail.com>
2022-06-30 23:03:05 -0400

    fire function

--- battleship.js
+++ battleship.js
@@ -15,12 +15,18 @@ var view = {
  var model = {
     boardSize: 7,
     numShips: 3,
+    shipsSunk: 0,
     shipLength: 3,
     shipsSunk: 0,
 
-    ships: [{locations: ["06", '16", "26"], hits: ["", "", ""] },
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""] },
             {locations: ["24", "34", "44"], hits: ["", "", ""] },
-            {locations: ["10", "11", "12"], hits: ["", "", ""] }}
+            {locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    fire: function(guess) {
+        for (var i =0; i< this.numShips; i++) {
+            var ship = this.ships[i];
+        }
+    }
 
  };
 view.displayMiss("00");
